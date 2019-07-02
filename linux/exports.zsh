eedfc701ad184fc6ff189a8fc3d321d2f34c005d
Trevor Murphy <murphtrevor@gmail.com>
2019-07-02 08:18:42 -0500

    Add pyenv to exports

--- linux/exports.zsh
+++ linux/exports.zsh
@@ -28,3 +28,13 @@ export MANPAGER='less -X';
 
 # PATH
 export PATH="/usr/local/bin:/usr/local/sbin:~/bin:$PATH"
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# PYENV
+export PATH="/home/aperture/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
